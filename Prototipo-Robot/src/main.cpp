2ab003b09c593400dfeaaa852545a85d5d0a6c32
R2D2 Robobot <arturo.cabg07@gmail.com>
2026-01-21 01:30:23 -0600

    Avance 1 en Main prototipo robot

--- Prototipo-Robot/src/main.cpp
+++ Prototipo-Robot/src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
+#include <cmath>
+#include "motores.h"
+#include "BNO.h"
+#include "PID.h"
+#include "constantes.h"
 
-// put function declarations here:
-int myFunction(int, int);
+//Incluir variables globales
+BNO055 bno;
+
+
+Motors motorss(
+    MOTOR1_PWM, MOTOR1_IN1, MOTOR1_IN2,
+    MOTOR2_PWM, MOTOR2_IN1, MOTOR2_IN2,
+    MOTOR3_PWM, MOTOR3_IN1, MOTOR3_IN2,
+    MOTOR4_PWM, MOTOR4_IN1, MOTOR4_IN2);
+
+//Declaración de Funciones
+void check_4Line(void);
+
+void process_serialD(void);//Procesar los datos seriales
+
+void process_serialD2(void);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  bno.InitializeBNO();
+  motorss.InitializeMotors();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
